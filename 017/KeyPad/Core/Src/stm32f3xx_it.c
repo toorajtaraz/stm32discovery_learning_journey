9f9df1491970c3ecac84e7d6913c7869c36c0b1b
Tooraj Taraz <tooraj.info@gmail.com>
2022-02-09 21:34:05 +0330

    adding assignment S9T2

---
+++ 017/KeyPad/Core/Src/stm32f3xx_it.c
@@ -0,0 +1,389 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f3xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f3xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "LiquidCrystal.h"
+#include "stdio.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define NOTPRESSED 128
+
+#define SAFE 0
+#define NOTSAFE 1
+
+#define LEDCOUNT 8
+
+#define INACTIVE 6
+#define LOCKED 0
+#define UNLOCKED 1
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+uint8_t sw = NOTPRESSED;
+uint8_t safety_sw = SAFE;
+int leds[] = {LD3_Pin, LD10_Pin,
+              LD5_Pin, LD8_Pin,
+              LD6_Pin, LD7_Pin,
+              LD9_Pin, LD4_Pin};
+
+RTC_TimeTypeDef now_alarm;
+RTC_DateTypeDef now_date_alarm;
+unsigned char prev_value = '-';
+uint8_t clear_flag = 0;
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern TIM_HandleTypeDef htim3;
+extern TIM_HandleTypeDef htim4;
+/* USER CODE BEGIN EV */
+extern RTC_HandleTypeDef hrtc;
+extern RTC_TimeTypeDef now;
+extern RTC_DateTypeDef now_date;
+extern uint8_t cur_pressed[];
+extern uint8_t pass_state;
+extern uint8_t lock_state;
+extern uint8_t pass_pointer;
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+  while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Pre-fetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles System service call via SWI instruction.
+  */
+void SVC_Handler(void)
+{
+  /* USER CODE BEGIN SVCall_IRQn 0 */
+
+  /* USER CODE END SVCall_IRQn 0 */
+  /* USER CODE BEGIN SVCall_IRQn 1 */
+
+  /* USER CODE END SVCall_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Pendable request for system service.
+  */
+void PendSV_Handler(void)
+{
+  /* USER CODE BEGIN PendSV_IRQn 0 */
+
+  /* USER CODE END PendSV_IRQn 0 */
+  /* USER CODE BEGIN PendSV_IRQn 1 */
+
+  /* USER CODE END PendSV_IRQn 1 */
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+  /* USER CODE BEGIN SysTick_IRQn 0 */
+
+  /* USER CODE END SysTick_IRQn 0 */
+  HAL_IncTick();
+  /* USER CODE BEGIN SysTick_IRQn 1 */
+
+  /* USER CODE END SysTick_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32F3xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32f3xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles EXTI line0 interrupt.
+  */
+void EXTI0_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI0_IRQn 0 */
+
+  /* USER CODE END EXTI0_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_0);
+  /* USER CODE BEGIN EXTI0_IRQn 1 */
+
+  /* USER CODE END EXTI0_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line3 interrupt.
+  */
+void EXTI3_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI3_IRQn 0 */
+
+  /* USER CODE END EXTI3_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_3);
+  /* USER CODE BEGIN EXTI3_IRQn 1 */
+  HAL_RTC_GetTime(&hrtc, &now_alarm, RTC_FORMAT_BIN);
+  HAL_RTC_GetDate(&hrtc, &now_date_alarm, RTC_FORMAT_BIN);
+  safety_sw = NOTSAFE;
+  /* USER CODE END EXTI3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line4 interrupt.
+  */
+void EXTI4_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI4_IRQn 0 */
+
+  /* USER CODE END EXTI4_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_4);
+  /* USER CODE BEGIN EXTI4_IRQn 1 */
+
+  /* USER CODE END EXTI4_IRQn 1 */
+}
+
+/**
+  * @brief This function handles EXTI line[9:5] interrupts.
+  */
+void EXTI9_5_IRQHandler(void)
+{
+  /* USER CODE BEGIN EXTI9_5_IRQn 0 */
+
+  /* USER CODE END EXTI9_5_IRQn 0 */
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_5);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_6);
+  HAL_GPIO_EXTI_IRQHandler(GPIO_PIN_7);
+  /* USER CODE BEGIN EXTI9_5_IRQn 1 */
+
+  /* USER CODE END EXTI9_5_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM3 global interrupt.
+  */
+void TIM3_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM3_IRQn 0 */
+  HAL_RTC_GetTime(&hrtc, &now, RTC_FORMAT_BIN);
+  HAL_RTC_GetDate(&hrtc, &now_date, RTC_FORMAT_BIN);
+  if(clear_flag == 1 || now.Seconds == 0) {
+    clear();
+    clear_flag = 0;
+  }
+  /* USER CODE END TIM3_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim3);
+  /* USER CODE BEGIN TIM3_IRQn 1 */
+  //LCD REFRESH
+  unsigned char time[30] = {'\0'};
+  unsigned char date[30] = {'\0'};
+  setCursor(0, 0);
+  sprintf(time,"%d:%d:%d", now.Hours, now.Minutes, now.Seconds);
+  print(time);
+  setCursor(10, 0);
+  switch(now_date.WeekDay) {
+    case RTC_WEEKDAY_FRIDAY:
+      print("FRIDAY");
+      break;
+    case RTC_WEEKDAY_SATURDAY:
+      print("SATURDAY");
+      break;
+    case RTC_WEEKDAY_SUNDAY:
+      print("SUNDAY");
+      break;
+    case RTC_WEEKDAY_MONDAY:
+      print("MONDAY");
+      break;
+    case RTC_WEEKDAY_TUESDAY:
+      print("TUESDAY");
+      break;
+    case RTC_WEEKDAY_WEDNESDAY:
+      print("WEDNESDAY");
+      break;
+    case RTC_WEEKDAY_THURSDAY:
+      print("THURSDAY");
+      break;
+    default:
+      break;
+  }
+  setCursor(0, 1);
+  sprintf(date, "%d/%d/%di=%d", now_date.Year, now_date.Month, now_date.Date, pass_pointer);
+  print(date);
+  setCursor(12, 1);
+  switch(lock_state) {
+    case LOCKED:
+      print("LOCKED");
+      break;
+    default:
+      print("UNLOCKED");
+      break;
+  }
+  if (safety_sw == NOTSAFE && lock_state == LOCKED) {
+    unsigned char time_alarm[30] = {'\0'};
+    unsigned char date_alarm[30] = {'\0'};
+    sprintf(time_alarm,"ALARM **%d:%d:%d**", now_alarm.Hours, now_alarm.Minutes, now_alarm.Seconds);
+    sprintf(date_alarm, "ALARM **%d/%d/%d**", now_date_alarm.Year, now_date_alarm.Month, now_date_alarm.Date);
+    setCursor(0, 2);
+    print(time_alarm);
+    setCursor(0, 3);
+    print(date_alarm);
+  }
+  //display();
+  /* USER CODE END TIM3_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM4 global interrupt.
+  */
+void TIM4_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM4_IRQn 0 */
+
+  /* USER CODE END TIM4_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim4);
+  /* USER CODE BEGIN TIM4_IRQn 1 */
+  //LED REFRESH
+  if(safety_sw == NOTSAFE && lock_state == LOCKED) {
+    for(int i = 0; i < LEDCOUNT; i++) {
+      HAL_GPIO_TogglePin(GPIOE, leds[i]);
+    }
+  } else {
+    for(int i = 0; i < LEDCOUNT; i++) {
+      HAL_GPIO_WritePin(GPIOE, leds[i], 0);
+    }
+  }
+  /* USER CODE END TIM4_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
